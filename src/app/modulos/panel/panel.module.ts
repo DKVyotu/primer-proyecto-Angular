ed056231ed09c4e007cef767125ac88c8c7e02f5
Antonio Hurtado <DKVantonio@gmail.com>
2025-04-24 21:53:24 -0500

    agregando lista del menu desplegable

--- src/app/modulos/panel/panel.module.ts
+++ src/app/modulos/panel/panel.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PanelRoutingModule } from './panel-routing.module';
-import { PanelComponent } from './panel.component'; 
+import { PanelComponent } from './panel.component';
 import { TablaAlumnosComponent } from './paginas/alumnos/componentes/tabla-alumnos/tabla-alumnos.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NavMenuComponent } from './componentes/nav-menu/nav-menu.component';
+
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    PanelComponent, 
-    TablaAlumnosComponent, 
-  ],
+  declarations: [PanelComponent, TablaAlumnosComponent, NavMenuComponent],
   imports: [
     CommonModule,
     PanelRoutingModule,
@@ -22,6 +22,7 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
   ],
   exports: [PanelComponent],
 })
