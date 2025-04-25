2028fd3488961301dae7ecc1676d220ea761ccd1
Antonio Hurtado <DKVantonio@gmail.com>
2025-04-25 00:57:08 -0500

    vinculando botones y pase de informacion

--- src/app/modulos/panel/paginas/alumnos/alumnos.component.ts
+++ src/app/modulos/panel/paginas/alumnos/alumnos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alumno } from './modelos';
 
 @Component({
   selector: 'app-alumnos',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AlumnosComponent {
 
+  listaAlumnos: Alumno[] = [
+    { id: 1, nombre: 'Juan', apellido: 'Perez', aprobado: true },
+    { id: 2, nombre: 'Maria', apellido: 'Lopez', aprobado: false },
+    { id: 3, nombre: 'Carlos', apellido: 'Sanchez', aprobado: true },
+    { id: 4, nombre: 'Ana', apellido: 'Gomez', aprobado: true },
+    { id: 5, nombre: 'Luis', apellido: 'Fernandez', aprobado: false },
+    { id: 6, nombre: 'Sofia', apellido: 'Martinez', aprobado: true },
+    { id: 7, nombre: 'Diego', apellido: 'Torres', aprobado: false },
+    { id: 8, nombre: 'Lucia', apellido: 'Ramirez', aprobado: true },
+    { id: 9, nombre: 'Jorge', apellido: 'Herrera', aprobado: false },
+    { id: 10, nombre: 'Valeria', apellido: 'Cruz', aprobado: true },
+  ];
+
+  eliminarAlumno(id: number) {
+    console.log(`Eliminar alumno con ID: ${id}`);
+    /* this.alumnos = this.ListaAlumnos.filter(alumno => alumno.id !== id); */
+  }
+  editarAlumno(alumno: Alumno) {
+    console.log(`Editar alumno: ${alumno.id}`);
+  }
 }
--- src/app/modulos/panel/paginas/alumnos/componentes/tabla-alumnos/tabla-alumnos.component.ts
+++ src/app/modulos/panel/paginas/alumnos/componentes/tabla-alumnos/tabla-alumnos.component.ts
@@ -1,19 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Alumno } from '../../modelos';
-
-
-const ListaAlumnos: Alumno[] = [
-  { id: 1, nombre: 'Juan', apellido: 'Perez', aprobado: true },
-  { id: 2, nombre: 'Maria', apellido: 'Lopez', aprobado: false },
-  { id: 3, nombre: 'Carlos', apellido: 'Sanchez', aprobado: true },
-  { id: 4, nombre: 'Ana', apellido: 'Gomez', aprobado: true },
-  { id: 5, nombre: 'Luis', apellido: 'Fernandez', aprobado: false },
-  { id: 6, nombre: 'Sofia', apellido: 'Martinez', aprobado: true },
-  { id: 7, nombre: 'Diego', apellido: 'Torres', aprobado: false },
-  { id: 8, nombre: 'Lucia', apellido: 'Ramirez', aprobado: true },
-  { id: 9, nombre: 'Jorge', apellido: 'Herrera', aprobado: false },
-  { id: 10, nombre: 'Valeria', apellido: 'Cruz', aprobado: true },
-];
 
 
 @Component({
@@ -25,6 +11,13 @@ const ListaAlumnos: Alumno[] = [
 export class TablaAlumnosComponent {
 
   displayedColumns: string[] = ['id', 'nombre', 'apellido', 'aprobado', 'acciones'];
-  dataSource = ListaAlumnos;
 
+  @Input()
+  lista: Alumno[] = [];
+
+  @Output() 
+  eliminarAlumno = new EventEmitter<number>();
+
+  @Output()
+  editarAlumno = new EventEmitter<Alumno>();
 }
